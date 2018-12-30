9881a0a51fec2fe48e0a84ce4bc5d7d1c46ca0ec
Thomas McNiven <hello@vevix.net>
2018-12-30 10:07:32 +1000

    v1.2

---
+++ CHANGELOG.md
@@ -0,0 +1,26 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## 1.2 - 2018-12-30
+### Added
+- This changelog.
+- New icon representing sound being played.
+
+### Changed
+- Default icon is now the newly added icon.
+- Icon in browser frame now updates based on the audible state of the tab.
+- When muted, the icon in the browser frame persists between tab reloads.
+
+## 1.1 - 2018-10-27
+### Added
+- Muting of tabs via the browserAction item
+
+## 1.0 - 2018-10-27
+### Added
+- Initial release
+- Context menu allowing muting of individual tabs
+
--- background.js
+++ background.js
@@ -2,8 +2,51 @@ const CONTEXT_MENU_ID = 'dc82bba0-dfd0-484d-9782-c8a27c521121';
 const MUTE_TAB_STR = 'Mute Tab';
 const UNMUTE_TAB_STR = 'Unmute Tab';
 const MUTED_BADGE_STR = ' M ';
+const BADGE_BACKGROUND = '#212121';
+const EXTENSION_ICONS = Object.freeze({
+  muted: {
+    '16': 'images/16_m.png',
+    '48': 'images/48_m.png',
+    '128': 'images/128_m.png',
+  },
+  unmuted: {
+    '16': 'images/16_u.png',
+    '48': 'images/48_u.png',
+    '128': 'images/128_u.png',
+  },
+});
+const MUTED_ICONS = EXTENSION_ICONS.muted;
+const UNMUTED_ICONS = EXTENSION_ICONS.unmuted;
 
-// 'Mute Tab' context menu
+// Listen for tab switches and update the browserAction icon according to audible state
+chrome.tabs.onActivated.addListener(function ({ tabId }) {
+  chrome.tabs.get(tabId, function (tab) {
+    if (tab.mutedInfo.muted) { return; }
+    let path = tab.audible ? UNMUTED_ICONS : MUTED_ICONS;
+    chrome.browserAction.setIcon({ path, tabId: tab.id });
+  });
+});
+
+// Listen for tab updates and update the icon to reflect the new state of the tab
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  // Keep the muted icon in sync if the tab was reloaded
+  if ('status' in changeInfo && tab.mutedInfo.muted) {
+    updateUIState(tabId, tab.mutedInfo.muted);
+  }
+
+  // If we're currently muted or about to be muted, then exit early
+  if ((tab.mutedInfo.muted && !('mutedInfo' in changeInfo)) || ('mutedInfo' in changeInfo && changeInfo.mutedInfo.muted)) {
+    return;
+  }
+
+  // Otherwise, check audible state and update icon accordingly.
+  if ('audible' in changeInfo) {
+    let path = changeInfo.audible ? UNMUTED_ICONS : MUTED_ICONS;
+    chrome.browserAction.setIcon({ path, tabId });
+  }
+});
+
+// Create context menu on all available menus
 chrome.contextMenus.create({
   id: CONTEXT_MENU_ID,
   type: 'normal',
@@ -11,34 +54,38 @@ chrome.contextMenus.create({
   contexts: ['all'] // Chrome doesn't allow us to add a custom context menu item to the actual tab.
 });
 
-chrome.contextMenus.onClicked.addListener(function(info, tab) {
+// Modify the mute state when interacting with the context menu option
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
   if (info.menuItemId === CONTEXT_MENU_ID) {
     updateMuteState(tab);
   }
 });
 
-// Chrome frame extension icon
-chrome.browserAction.setBadgeBackgroundColor({ color: '#212121' });
-chrome.browserAction.onClicked.addListener(function(tab) {
+// Browser frame icon
+chrome.browserAction.setBadgeBackgroundColor({ color: BADGE_BACKGROUND });
+chrome.browserAction.onClicked.addListener(function (tab) {
   updateMuteState(tab);
 });
 
 // Helper functions
 function updateMuteState(tab) {
-  let muted = muteTab(tab);
-  let title = MUTE_TAB_STR;
-  if (muted) {
-    title = UNMUTE_TAB_STR;
-    chrome.browserAction.setBadgeText({ text: MUTED_BADGE_STR, tabId: tab.id });
-  } else {
-    chrome.browserAction.setBadgeText({ text: '', tabId: tab.id });
-  }
-  chrome.contextMenus.update(CONTEXT_MENU_ID, { title });
+  // Flip the muted state of the tab
+  let current_state = tab.mutedInfo.muted;
+  let is_muted = !current_state;
+  chrome.tabs.update(tab.id, { muted: is_muted });
+  updateUIState(tab.id, is_muted);
 }
 
-function muteTab(tab) {
-  let current_state = tab.mutedInfo.muted;
-  let new_state = !current_state;
-  chrome.tabs.update(tab.id, { muted: new_state });
-  return new_state;
+function updateUIState(tabId, is_muted) {
+  // Update the UI to reflect the current muted state
+  let title = MUTE_TAB_STR;
+  if (is_muted) {
+    title = UNMUTE_TAB_STR;
+    chrome.browserAction.setBadgeText({ text: MUTED_BADGE_STR, tabId });
+    chrome.browserAction.setIcon({ path: MUTED_ICONS, tabId });
+  } else {
+    chrome.browserAction.setBadgeText({ text: '', tabId });
+    chrome.browserAction.setIcon({ path: UNMUTED_ICONS, tabId });
+  }
+  chrome.contextMenus.update(CONTEXT_MENU_ID, { title });
 }
